21e4b0334eac7520a73ec61f476425b3eb9af6a6
Josh Smith <cmyuiosu@gmail.com>
2019-10-03 18:02:10 -0400

    theres a memleak somewhere in this lol

--- pp_98.c
+++ pp_98.c
@@ -1,16 +1,13 @@
 // A Simple (and pretty beginner-level coded) map/pp finder made by cmyui as (basically) his first project in C.
-#define _CRT_SECURE_NO_WARNINGS
+//#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <mysql/mysql.h>
 #include <stdlib.h>
-#include <string.h>
+//#include <string.h>
 
-#include </mnt/d/Development/Misc Tools/pp_98/oppai.h>
+#include </mnt/d/Development/Misc Tools/gen_table/oppai.h>
 
-#define DEBUG 0
-
-#define LogError(x) printf(KRED "ERROR CODE "#x KRESET "\n")
-
+// Console colours
 #define KNRM  "\x1B[0m"
 #define KRED  "\x1B[31m"
 #define KGRN  "\x1B[32m"
@@ -22,16 +19,28 @@
 #define KRESET "\033[0m"
 #define NULL_TERM '\0'
 
+#define LogError(x) printf(KRED "ERROR CODE "#x KRESET "\n")
+
 // TODO: config
+#define DEBUG 0
+
+// SQL
 #define SQL_SERVER "localhost"
 #define SQL_USER "root"
 #define SQL_PASSWD "no"
 #define SQL_DB "ripple"
-#define __BeatmapPath "/mnt/d/Development/Misc Tools/pp_98/maps/"
-#define ACCURACY 98.f
-#define MAX_PP 4000.f
-#define MIN_PP 1500.f
-#define SQLInsert_Max_Len 112
+
+// Path to beatmaps folder
+#define __BeatmapPath "/mnt/d/Development/Misc Tools/gen_table/maps/"
+
+const size_t path_max_size = sizeof(__BeatmapPath) + 11; // max size of beatmap path
+const size_t sql_max_size = sizeof("INSERT IGNORE INTO pp_table(id,beatmap_id,pp,star_rating,pp_ratio)VALUES(NULL,1234567,1234.123,12.12345,123);"); // max size of sql insert to pp_table
+char MapPath[sizeof(__BeatmapPath) + 11] = { '\0' };
+char SQL[sizeof("INSERT IGNORE INTO pp_table(id,beatmap_id,pp,star_rating,pp_ratio)VALUES(NULL,1234567,1234.123,12.12345,123);")] = { '\0' };
+
+// Settings
+#define MAX_PP 2750.f
+#define MIN_PP 1000.f
 
 enum Mods
 {
@@ -72,14 +81,8 @@ enum Mods
 	TimeAltering = DoubleTime | HalfTime | Nightcore
 };
 
-unsigned char _Query(MYSQL* conn, char Query[]) {
-    if (mysql_query(conn, Query)) {
-        fprintf(stderr, KRED "%s" KRESET "\n", mysql_error(conn));
-        return 0;
-    } return 1;
-}
-
-unsigned char GetFileSize(const char* BeatmapPath) {
+unsigned char FileExists(const char* BeatmapPath)
+{
 	FILE *f = fopen(BeatmapPath, "r");
     if (!f) return 0;
 
@@ -92,26 +95,45 @@ unsigned char GetFileSize(const char* BeatmapPath) {
     return 1;
 }
 
-int main(void) {
+int main(int argc, char *argv[])
+{
+    // Default mods
+    int mods = Relax | HardRock | Hidden | DoubleTime;
+    float accuracy = 100.f;
 
-    /* Setup connection with MySQL */
-    MYSQL *conn;
-    MYSQL *_conn; // TODO: remove
-    MYSQL_RES *res;
-    MYSQL_ROW row;
-
-    conn = mysql_init(NULL);
-    if (!mysql_real_connect(conn, SQL_SERVER, SQL_USER, SQL_PASSWD, SQL_DB, 0, NULL, 0)) {
-        fprintf(stderr, "%s\n", mysql_error(conn));
+    // TODO: actually properly do these lol.
+    // Also, I can add quite a bit of cool flag things here!
+    if (argc > 1) mods     = atoi(argv[1]);
+    if (argc > 2) accuracy = atof(argv[2]);
+    if (!mods || mods > 1073741824 || !accuracy || accuracy > 100.f)
+    {
+        printf("\n" KRED "Invalid custom settings (%i, %.2f%%)\n" KRESET "\n", mods, accuracy);
         return 0;
     }
 
-    if (!_Query(conn, "SELECT beatmap_id FROM beatmaps WHERE ranked IN (-2, 2) ORDER BY beatmap_id ASC")) return 0;
+    printf(KYEL "Settings:\n" KCYN "Mods: %i\nAccuracy: %.2f" KRESET "\n\n", mods, accuracy);
+
+    MYSQL *conn;
+    MYSQL_RES *res;
+    MYSQL_ROW row;
+    conn = mysql_init(NULL);
+
+    if (!mysql_real_connect(conn, SQL_SERVER, SQL_USER, SQL_PASSWD, SQL_DB, 0, NULL, 0))
+    {
+        fprintf(stderr, KRED "%s" KRESET "\n", mysql_error(conn));
+        return 0;
+    }
+
+    if (mysql_query(conn, "SELECT beatmap_id FROM beatmaps WHERE ranked IN (-2, 2) ORDER BY beatmap_id ASC"))
+    {
+        fprintf(stderr, KRED "%s" KRESET "\n", mysql_error(conn));
+        return 0;
+    }
 
     res = mysql_store_result(conn);
 
     unsigned long long row_count = mysql_num_rows(res);
-    printf(KCYN "Beatmaps found (database): %llu" KRESET "\n", row_count);
+    printf(KCYN "Found %llu beatmaps matching criteria. Calculating PP values.." KRESET "\n", row_count);
 
     // Build beatmap array
     int* BeatmapArray = calloc(row_count, sizeof(int));
@@ -120,139 +142,72 @@ int main(void) {
 
     mysql_free_result(res);
 
-    char BeatmapID[8];
-    char MapPath[strlen(__BeatmapPath) + 11];// TODO: const size
-    float PP, MapStars;
-
-    for (int i = 0; i <= row_count; i++) {
+    for (int i = 0; i <= row_count; i++)
+        {
         ezpp_t ez = ezpp_new();
-        if (!ez) {
-            printf(KRED "\n** Failed to load ezpp. **" KRESET "\n");
+
+        if (!ez)
+        {
+            printf("\n" KRED "** Failed to load ezpp. **" KRESET "\n");
             return 0;
         }
 
-        ezpp_set_mods(ez, Relax + HardRock + Hidden + DoubleTime);
-        ezpp_set_nmiss(ez, 0);
-        ezpp_set_accuracy_percent(ez, ACCURACY);
-        //ezpp_set_combo(ez, sData.MaxCombo);
-        ezpp_set_mode(ez, 0);
+        // oppai settings.
+        ezpp_set_mods             (ez, mods);
+        ezpp_set_accuracy_percent (ez, accuracy);
+        /*
+        ezpp_set_nmiss            (ez, 0);
+        ezpp_set_mode             (ez, 0);
+        ezpp_set_combo            (ez, 0);
+        */
 
-#if DEBUG
-        printf(KMAG "\nBeatmapArray[i]: %i\ni: %i" KRESET "\n", BeatmapArray[i], i);
-#endif
-        sprintf(BeatmapID, "%d", BeatmapArray[i]);
-        BeatmapID[7] = NULL_TERM;
+        snprintf(MapPath, path_max_size, "/mnt/d/Development/Misc Tools/gen_table/maps/%i.osu", BeatmapArray[i]);
 
-        strcat(MapPath, __BeatmapPath);
-        strcat(MapPath, BeatmapID);
-        strcat(MapPath, ".osu");
-
-        if (!GetFileSize(MapPath)) {
-#if DEBUG
-            printf(KRED "\nFailed %s" KRESET "\n", BeatmapID);
-#endif
-            strcpy(MapPath, "\0");
+        if (!FileExists(MapPath))
             continue;
-        }
 
         // Init oppai's ezpp with the map's path.
         ezpp(ez, MapPath);
-        strcpy(MapPath, "\0");
-
-        /* ez->pp and ez->stars?
-        PP = ezpp_pp(ez);
-        MapStars = ezpp_stars(ez);
-        */
 
 #if DEBUG
-        printf("PP: %.2f\n", ez->pp);
+        printf("PP: %.3f\n", ez->pp);
 #endif
 
-        if (ez->pp < MIN_PP || ez->pp > MAX_PP || is_nan(ez->pp)) { // oppai occasionally returns a nan value.. very cool kanye
-#if DEBUG
-            printf(KRED "\nBAD Failed %s (%.2fpp)" KRESET "\n", BeatmapID, ez->pp);
-#endif
+        if (ez->pp < MIN_PP || ez->pp > MAX_PP || is_nan(ez->pp))
             continue;
-        }
 
-        //TODO: sql prepare statement?
-        char SQLInsert[SQLInsert_Max_Len] = { '\0' };
-        strcat(SQLInsert, "INSERT IGNORE INTO daniel_maps(id,beatmap_id,pp,star_rating,pp_ratio)VALUES(NULL,");
+        { // Accepted val
+            int pp_ratio = (int)((ez->speed_pp / (ez->aim_pp + ez->speed_pp + ez->acc_pp)) * 100); // In Akatsuki pp, ez->speed_pp isnt part of ez->pp.
 
-        strcat(SQLInsert, BeatmapID);
-        strcat(SQLInsert, ",");
-
-        static char
-            _PP[8] = { '\0' },
-            _SR[8] = { '\0' }
-        ;
-
-        // PP
-        gcvt(ez->pp, 7, _PP);
-        _PP[7] = NULL_TERM;
-        strcat(SQLInsert, _PP);
-        strcat(SQLInsert, ",");
-
-        // SR
-        gcvt(ez->stars, 8, _SR);
-        _SR[7] = NULL_TERM;
-        strcat(SQLInsert, _SR);
-        strcat(SQLInsert, ",");
-
-        char pp_ratio[5];
-        gcvt((int)((ez->speed_pp / ez->pp) * 100), 4, pp_ratio);
-        pp_ratio[4] = NULL_TERM;
-        strcat(SQLInsert, pp_ratio);
-        strcat(SQLInsert, ");");
-
-        if (!_Query(conn, SQLInsert)) return 0;
-
-        printf(
-            KGRN "\n"
-            "Inserting accepted value" KRESET "\n"
-            "Beatmap ID: %s\n"
-            "PP: %spp (%fpp)\n"
-            "Star Rating: %s*\n"
-            "Stream pp: %s%%\n",
-            BeatmapID,
-            _PP,
-            ez->pp,
-            _SR,
-            pp_ratio
-        );
-
-#if DEBUG
-        printf(
-            "SQLInsert:\n"
-            "len: %zu\n"
-            "str: \"%s\"\n",
-            strlen(SQLInsert),
-            SQLInsert
-        );
-#endif
-
-        if (strlen(SQLInsert) > SQLInsert_Max_Len)
-        {
-            printf(
-                KRED "\n\n"
-                "SQLInsert length too large for variable, possibly memory leaking!\n"
-                "Maxlen: %i\n"
-                "Len: %zu\n"
-                "String: \"%s\"" KRESET "\n",
-                SQLInsert_Max_Len,
-                strlen(SQLInsert),
-                SQLInsert
+            snprintf(
+                SQL,
+                sql_max_size,
+                "INSERT IGNORE INTO pp_table(id,beatmap_id,pp,star_rating,pp_ratio)VALUES(NULL,%i,%.3f,%.5f,%i);",
+                BeatmapArray[i],
+                ez->pp,
+                ez->stars,
+                pp_ratio
             );
-            return 0;
+
+            if (mysql_query(conn, SQL))
+            {
+                fprintf(stderr, KRED "%s" KRESET "\n", mysql_error(conn));
+                return 0;
+            }
+
+            printf(
+                "\n" KGRN
+                "Accepted value." KRESET "\n"
+                "Beatmap ID: %i\n"
+                "PP: %.3fpp\n"
+                "Star Rating: %.5f*\n"
+                "Stream pp: %i%%\n",
+                BeatmapArray[i],
+                ez->pp,
+                ez->stars,
+                pp_ratio
+            );
         }
-
-        // TODO: find out how to actually do this?
-        //strcpy(SQLInsert, "\0");
-        //strcpy(_SR, "\0");
-        //strcpy(_PP, "\0");
-
-        //PP = 0.f;
-        //MapStars = 0.f;
 
         ezpp_free(ez);
     }
