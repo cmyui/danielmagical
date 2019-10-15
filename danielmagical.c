c184b6c7071dbf7b74ccf5f16bea35b14aa922fb
Josh <cmyuisuki@gmail.com>
2019-10-15 05:04:21 -0400

    Some extra cleaning..

--- danielmagical.c
+++ danielmagical.c
@@ -8,58 +8,38 @@
 // "Quiets" the program if disabled..
 #define VERBOSE 1
 
-char BEATMAP_FOLDER[17] = { '\0' },
-     SQL[sizeof("INSERT IGNORE INTO pp_table(id,beatmap_id,pp,star_rating,pp_ratio)VALUES(NULL,1234567,1234.123,12.12345,123);")] = { '\0' };
+#define SQL_MAX_LEN sizeof("INSERT IGNORE INTO pp_table(id,beatmap_id,pp,star_rating,pp_ratio)VALUES(NULL,1234567,1234.123,12.12345,123);")
+
+char BEATMAP_FOLDER[17] = {'\0'},
+     SQL[SQL_MAX_LEN]   = {'\0'};
 
 
 // Console colours
-#define KNRM      "\x1B[0m"
-#define KRED      "\x1B[31m"
-#define KGRN      "\x1B[32m"
-#define KYEL      "\x1B[33m"
-#define KBLU      "\x1B[34m"
-#define KMAG      "\x1B[35m"
-#define KCYN      "\x1B[36m"
-#define KGRY      "\x1B[37m"
-#define KRESET    "\033[0m"
+#define KNRM   "\x1B[0m"
+#define KRED   "\x1B[31m"
+#define KGRN   "\x1B[32m"
+#define KYEL   "\x1B[33m"
+#define KBLU   "\x1B[34m"
+#define KMAG   "\x1B[35m"
+#define KCYN   "\x1B[36m"
+#define KGRY   "\x1B[37m"
+#define KRESET "\033[0m"
 
 
 enum Mods {
-    noMod             = 0,
-    NoFail            = 1 << 0,
-    Easy              = 1 << 1,
-  //NoVideo           = 1 << 2,
-    Hidden            = 1 << 3,
-    HardRock          = 1 << 4,
-  //SuddenDeath       = 1 << 5,
-    DoubleTime        = 1 << 6,
-    Relax             = 1 << 7,
-    HalfTime          = 1 << 8,
-    Nightcore         = 1 << 9,
-    Flashlight        = 1 << 10,
-  //Autoplay          = 1 << 11,
-    SpunOut           = 1 << 12,
-  //Relax2            = 1 << 13,
-  //Perfect           = 1 << 14,
-  //Key4              = 1 << 15,
-  //Key5              = 1 << 16,
-  //Key6              = 1 << 17,
-  //Key7              = 1 << 18,
-  //Key8              = 1 << 19,
-  //FadeIn            = 1 << 20,
-  //Random            = 1 << 21,
-  //Cinema            = 1 << 22,
-  //Target            = 1 << 23,
-  //Key9              = 1 << 24,
-  //KeyCoop           = 1 << 25,
-  //Key1              = 1 << 26,
-  //Key3              = 1 << 27,
-  //Key2              = 1 << 28,
-  //LastMod           = 1 << 29,
-  //KeyMod            = Key1 | Key2 | Key3 | Key4 | Key5 | Key6 | Key7 | Key8 | Key9 | KeyCoop,
-  //FreeModAllowed    = NoFail | Easy | Hidden | HardRock | SuddenDeath | Flashlight | FadeIn | Relax | Relax2 | SpunOut | KeyMod,
-  //ScoreIncreaseMods = Hidden | HardRock | DoubleTime | Flashlight | FadeIn,
-  //TimeAltering      = DoubleTime | HalfTime | Nightcore
+    noMod       = 0,
+    NoFail      = 1 << 0,
+    Easy        = 1 << 1,
+    TouchDevice = 1 << 2,
+    Hidden      = 1 << 3,
+    HardRock    = 1 << 4,
+    DoubleTime  = 1 << 6,
+    Relax       = 1 << 7,
+    HalfTime    = 1 << 8,
+    Nightcore   = 1 << 9,
+    Flashlight  = 1 << 10,
+    SpunOut     = 1 << 12,
+  //Relax2      = 1 << 13,
 };
 
 
